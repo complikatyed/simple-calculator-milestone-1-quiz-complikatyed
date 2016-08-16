a26c30b51850a558755e9169bafe579a56ee790c
Katye Russell <Katye.Russell@gmail.com>
2016-08-15 17:38:25 -0700

    Set up files for Expression and ExpressionTest and begin preparing tests

---
+++ SimpleCalculator.Tests/ExpressionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class ExpressionTest
+    {
+        [TestMethod]
+        public void LHSCanBeExtracted()
+        {
+
+        }
+        [TestMethod]
+        public void RHSCanBeExtracted()
+        {
+
+        }
+
+        [TestMethod]
+        public void OperatorCanBeExtracted()
+        {
+
+        }
+
+    }
+}
