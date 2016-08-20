aa137f4d99e8cb0bfe3b33f4f98810dffb9e7b3a
Katye Russell <Katye.Russell@gmail.com>
2016-08-19 20:31:09 -0700

    Add 'Storage' class and tests to capture last answer and last query

---
+++ SimpleCalculator.Tests/StorageTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class StorageTest
+    {
+        Storage storage = new Storage();
+        string testRequest1 = "12 + 8";
+        int testAnswer1 = 4;
+
+        [TestMethod]
+        public void QueryStringIsStored()
+        {
+            string lastExpression = storage.storeLastQuery(testRequest1);
+            Assert.AreEqual("12 + 8", lastExpression);
+        }
+
+        [TestMethod]
+        public void AnswerStringIsStored()
+        {
+            int lastAnswer = storage.storeLastAnswer(testAnswer1);
+            Assert.AreEqual(4, lastAnswer);
+        }
+
+    }
+}
