1f137bcb37f929eaa554e23a571777c57af195a8
Katye Russell <Katye.Russell@gmail.com>
2016-08-20 11:49:55 -0700

    Change 'Storage' to 'Stack'

--- SimpleCalculator.Tests/StackTest.cs
+++ SimpleCalculator.Tests/StackTest.cs
@@ -4,23 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SimpleCalculator.Tests
 {
     [TestClass]
-    public class StorageTest
+    public class StackTest
     {
-        Storage storage = new Storage();
+        Stack stack = new Stack();
         string testRequest1 = "12 + 8";
         int testAnswer1 = 4;
 
         [TestMethod]
         public void QueryStringIsStored()
         {
-            string lastExpression = storage.storeLastQuery(testRequest1);
+            string lastExpression = stack.storeLastQuery(testRequest1);
             Assert.AreEqual("12 + 8", lastExpression);
         }
 
         [TestMethod]
         public void AnswerStringIsStored()
         {
-            int lastAnswer = storage.storeLastAnswer(testAnswer1);
+            int lastAnswer = stack.storeLastAnswer(testAnswer1);
             Assert.AreEqual(4, lastAnswer);
         }
 
--- SimpleCalculator/Stack.cs
+++ SimpleCalculator/Stack.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SimpleCalculator
 {
-    public class Storage : Expression
+    public class Stack : Expression
     {
         string lastQuery { get; set; }
         int lastAnswer { get; set; }
